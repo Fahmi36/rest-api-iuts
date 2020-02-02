393e303bdca44b547cfda23e7413b6947787eb8d
Fahmi36 <fauzifahmi55@gmail.com>
2020-02-03 00:17:33 +0700

    pdf

--- application/libraries/M_pdf.php
+++ application/libraries/M_pdf.php
@@ -1,23 +1,24 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 class m_pdf {
     function __construct(){
-    function m_pdf()
-    {
-        $CI = & get_instance();
-        log_message('Debug', 'mPDF class is loaded.');
+        parent::__construct();
     }
- 
-    function load($param=NULL)
-    {
-        include_once APPPATH.'/third_party/mpdf/mpdf.php';
-         
-        if ($params == NULL)
+        function m_pdf()
         {
-            $param = '"en-GB-x","A4","","",10,10,10,10,6,3';          		
+            $CI = & get_instance();
+            log_message('Debug', 'mPDF class is loaded.');
         }
-         
-        //return new mPDF($param);
-        return new mPDF();
-    }
-}
+     
+        function load($param=NULL)
+        {
+            include_once APPPATH.'/third_party/mpdf/mpdf.php';
+             
+            if ($params == NULL)
+            {
+                $param = '"en-GB-x","A4","","",10,10,10,10,6,3';          		
+            }
+             
+            //return new mPDF($param);
+            return new mPDF();
+        }
 }
