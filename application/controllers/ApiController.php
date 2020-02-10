19cad5ad15ac6e8e2ed2239d08f4804c5d552f8e
Fahmi36 <fauzifahmi55@gmail.com>
2020-02-11 01:28:07 +0700

    debug pbb

--- application/controllers/ApiController.php
+++ application/controllers/ApiController.php
@@ -46,17 +46,6 @@ class ApiController extends CI_Controller {
 		} else {
 			$data = json_decode($response);
 			foreach ($data as $key) {
-				if ($key->pesan == 'Data Tidak ditemukan') {
-					$json = json_encode(array('success'=>false,'msg'=>'Tidak ada Data'));
-				}else if ($key->pesan == 'Panjang Karakter Kurang dari 15') {
-					$json = json_encode(array('success'=>false,'msg'=>'Maaf Panjang Angka NIK Kurang dari 15 Angka'));
-                }else if ($key->errorCode == '32') {
-                    $json = json_encode(array('success'=>false,'msg'=>'Server Pajak Sedang Sibuk, Silakan Kirim Ulang'));
-                }else if ($key->errorCode == '99') {
-                    $json = json_encode(array('success'=>false,'msg'=>'Server Pajak Sedang Sibuk, Silakan Kirim Ulang'));
-                }else if ($key->errorCode == '4') {
-                    $json = json_encode(array('success'=>false,'msg'=>'Angka NIK / NPWP Kurang Dari 15'));
-                }else{
                 	if ($jns_pajak != null OR $jns_pajak != '') {
                 		if ($key->JNS_PAJAK == $jns_pajak) {
                 			if ($key->status == "TIDAK TERDAPAT TUNGGAKAN") {
@@ -81,7 +70,6 @@ class ApiController extends CI_Controller {
                 		}else{
                 			$json = json_encode(array('success'=>false,'msg'=>'Maaf NIK Anda Tidak Mempunyai Pajak PBB'));
                 		}
-                	}
                 }
 			echo $json;
 			}
