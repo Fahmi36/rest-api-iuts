d8f2fb975fcdc9af3d9e981d385aa3f01a61f0f9
Fahmi36 <fauzifahmi55@gmail.com>
2020-02-11 01:42:10 +0700

    debug pbb

--- application/controllers/ApiController.php
+++ application/controllers/ApiController.php
@@ -46,8 +46,8 @@ class ApiController extends CI_Controller {
 		} else {
 			$data = json_decode($response);
 			$json_errors = array();
-			foreach ($data as $key =>$value) {
-				return var_dump($json_errors[$value]);
+			foreach ($data as $key) {
+				return var_dump($json_errors[$key]);
 				if ($key->pesan == 'Data Tidak ditemukan') {
 					$json = json_encode(array('success'=>false,'msg'=>'Tidak ada Data'));
 				}else if ($key->pesan == 'Panjang Karakter Kurang dari 15') {
