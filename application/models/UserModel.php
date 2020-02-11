379ff463f8a5378a6234e6c63a61bc5a189cc27b
Fahmi36 <fauzifahmi55@gmail.com>
2020-02-11 17:56:48 +0700

    Detail pemohon teknis

--- application/models/UserModel.php
+++ application/models/UserModel.php
@@ -384,12 +384,14 @@ class UserModel extends CI_Model {
         $this->db->select('jarak_pasar.skor as skorjarakpasar, rencana_jalan.skor as skorrenjalan, jalan_eksisting.skor as skorjalaneksis, tata_ruang.skor as skortataruang, jarak_usaha.skor as skorjarakusaha, penggunaan_lahan.skor as skorpenglahan, ROUND(AVG(jarak_pasar.skor + rencana_jalan.skor + jalan_eksisting.skor + tata_ruang.skor + jarak_usaha.skor + penggunaan_lahan.skor),1) as skormanfaat ');
         $this->db->from('data_slf');
         $this->db->join('kondisi_slf', 'kondisi_slf.id_slf = data_slf.id_slf', 'INNER');
+        $this->db->join('data_iuts', 'data_iuts.id_slf = data_slf.id_slf', 'left');
+        $this->db->join('kondisi_iuts', 'kondisi_iuts.id_iuts = data_iuts.id_iuts', 'INNER');
         $this->db->join('admin_teknis', 'admin_teknis.id_bangunan = data_slf.id_slf', 'INNER');
 
         $this->db->join('jarak_pasar', 'jarak_pasar.id = admin_teknis.id_pasar', 'INNER');
         $this->db->join('rencana_jalan', 'rencana_jalan.id = admin_teknis.id_rencana', 'INNER');
         $this->db->join('jalan_eksisting', 'jalan_eksisting.id = admin_teknis.id_rencana_eksisting', 'INNER');
-        $this->db->join('tata_ruang', 'tata_ruang.id = kondisi_bangunan.id_tata_ruang', 'INNER');
+        $this->db->join('tata_ruang', 'tata_ruang.id = kondisi_iuts.id_tata_ruang', 'INNER');
         $this->db->join('jarak_usaha', 'jarak_usaha.id = admin_teknis.id_jarak', 'INNER');
         $this->db->join('penggunaan_lahan', 'penggunaan_lahan.id = admin_teknis.id_lahan', 'INNER');
         $this->db->where('data_slf.id_slf', $id_bangunan);
