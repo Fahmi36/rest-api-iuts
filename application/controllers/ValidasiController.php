f71cdf2a55f255a92c77f61b592fd05e61ae60a7
Fahmi36 <fauzifahmi55@gmail.com>
2020-02-18 15:36:13 +0700

    cobapush

--- application/controllers/ValidasiController.php
+++ application/controllers/ValidasiController.php
@@ -378,8 +378,8 @@ class ValidasiController extends CI_Controller {
                                         $json = $this->returnResultCustom(true,'Berhasil Simpan Data');
                                         $json['idslf'] = $slf;
                                         $json['idiuts'] = $iuts;
-                                        echo json_encode($json);
-                                        return;
+                                        // echo json_encode($json);
+                                        // return;
                                     // }else{
                                     //     $json = $this->returnResultCustom(false,'Gagal Masukan Foto IUTS');
                                     // }
