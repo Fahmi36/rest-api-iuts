7f34e82d9865829fd1cfc4a255fcd25b2d5ead4e
Fahmi36 <fauzifahmi55@gmail.com>
2020-03-05 03:06:36 +0700

    update barcode

--- application/controllers/OfficeController.php
+++ application/controllers/OfficeController.php
@@ -393,11 +393,13 @@ class OfficeController extends CI_Controller {
 		        	'ContactInfo' => site_url('/'),
     			);    		
     			$taut='https://perizinan.jakarta.go.id/'; 
-				$tcpdf->write2DBarcode($taut, 'QRCODE,H', 80,0,20,20);
+				$tcpdf->write2DBarcode($taut, 'QRCODE,H', 80,60,20,20);
       			$tcpdf->setSignature($results['cert'], $results['pkey'], 'AJ102938++!', '', 2, $info); 
       			$tcpdf->Image(base_url('assets/sertifikat/tte4.jpg'), 117, 201, 60, 18, 'PNG'); 
       			$tcpdf->setSignatureAppearance(117, 201, 60, 18);
       			$html .= '<br pagebreak="true"/>';
+		        $html .= $this->load->view('pages/klausul',$data, true);
+		        $tcpdf->WriteHTML($html);
 		        $html .= $this->load->view('pages/bawaberkas',$data, true);
 		        $tcpdf->WriteHTML($html,true,false,true,false,true,false,'');
       			// return var_dump($data['barcode']);
--- application/views/pages/suratsk.php
+++ application/views/pages/suratsk.php
@@ -77,35 +77,6 @@
 					NIP. ....................................</p>
 				</div>
 			</div>
-			<div style="font-family:Roboto-Regular,Helvetica,Arial,sans-serif;font-size:12pt;line-height:1.6;padding-left:20px;padding-right:20px;padding-bottom:12px;padding-top:24px;width: 100%;">
-				<p style="margin: 0;">Tembusan :</p>
-				<ol style="padding-left: 16px;margin: 0;">
-					<li>Kepala Dinas Koperasi, Usaha Mikro, Kecil dan Menengah, serta Perdagangan Provinsi DKI Jakarta;</li>
-					<li>Kepala Satuan Polisi Pamong Praja Provinsi DKI Jakarta;</li>
-					<li>Arsip.</li>
-				</ol>
-			</div>
-			<div style="font-family:Roboto-Regular,Helvetica,Arial,sans-serif;font-size:11pt;line-height:1.6;padding-left:20px;padding-right:20px;padding-bottom:12px;padding-top:24px">
-				<center><h4 style="margin: 0;"><b>KLAUSUL</b></h4></center>
-				<p>KEWAJIBAN</p>
-				<ol style="padding-left: 16px;">
-					<?php foreach ($kewajiban->result() as $key): ?>
-
-						<li><?=$key->deskripsi?></li>
-					<?php endforeach ?>
-
-				</ol>
-			</div>
-			<div style="font-family:Roboto-Regular,Helvetica,Arial,sans-serif;font-size:11pt;line-height:1.6;padding-left:20px;padding-right:20px;padding-bottom:12px;">
-				<p>Larangan</p>
-				<ol style="padding-left: 16px;">
-					<?php foreach ($larangan->result() as $larang): ?>
-
-						<li><?=$larang->deksripsi?></li>
-					<?php endforeach ?>
-
-				</ol>
-			</div>
 		</div>
 	</body>
 	</html>
